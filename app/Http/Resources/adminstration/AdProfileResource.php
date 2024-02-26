a67f92c0f4bb76aacf71116e3fb5533d8817b08d
mohamed ibrahim <112399700+muhamed-ibrahim@users.noreply.github.com>
2024-02-26 22:59:06 +0200

    first commit

---
+++ app/Http/Resources/adminstration/AdProfileResource.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Http\Resources\adminstration;
+
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class AdProfileResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @return array<string, mixed>
+     */
+    public function toArray(Request $request): array
+    {
+        return [
+            'name' => $this->name,
+            'email' => $this->email,
+            'image' => $this->image,
+            'phone' => $this->phone,
+            'adminstration name' => $this->adminstration()->first()->name,
+            'adminstration address' => $this->adminstration()->first()->address,
+
+        ];
+    }
+}
