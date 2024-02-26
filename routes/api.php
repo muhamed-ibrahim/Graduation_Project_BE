919101e2455a79afa177884548dc15f69d6bc2e1
mohamed ibrahim <112399700+muhamed-ibrahim@users.noreply.github.com>
2024-02-26 13:24:57 +0200

    init commit

---
+++ routes/api.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Api\adminstration\auth\AuthController;
+use App\Http\Controllers\Api\adminstration\AdminstrationController;
+use App\Http\Controllers\Api\adminstration\auth\PasswordController;
+use App\Http\Controllers\Api\adminstration\auth\ForgotPasswordController;
+
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register API routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "api" middleware group. Make something great!
+|
+*/
+
+
+Route::controller(AuthController::class)->group(function () {
+    Route::post('/login', 'login');
+    Route::post('/logout', 'logout')->middleware('auth:sanctum', 'adminstration_admin');
+});
+Route::post('forgot-password', [ForgotPasswordController::class, 'forgot']);
+Route::post('reset-password', [ForgotPasswordController::class, 'reset']);
+Route::group(['middleware' => 'auth:sanctum','adminstration_admin'], function () {
+    Route::get('/showProfile', [AdminstrationController::class, 'showProfile']);
+    Route::post('/updateProfile', [AdminstrationController::class, 'updateProfile']);
+    Route::post('/updatePassword', [PasswordController::class, 'updatePassword']);
+});
+
+
+
+
+
+
+// Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+//     return $request->user();
+// });
